7a2ca817db7fc6c9d629d563b4b3547838739cad
agiertli <agiertli@redhat.com>
2014-11-10 21:27:07 +0100

    fix another conversion typo

--- vaadin-frontend/src/main/java/org/fi/muni/diploma/thesis/utils/humantask/HumanTaskName.java
+++ vaadin-frontend/src/main/java/org/fi/muni/diploma/thesis/utils/humantask/HumanTaskName.java
@@ -9,7 +9,7 @@ package org.fi.muni.diploma.thesis.utils.humantask;
 public enum HumanTaskName {
 
 	DEPRECATE_SERVICE("Deprecate Service"), RETIRE_SERVICE("Retire Service"), INITIALIZE("Initialize"), REGISTER("Register"), TEST("Test"), IDENTIFY_SERVICE(
-			"Identify Service"), EVALUATE_TEST_RESULTS("Evaluate tests result"), SELECT_SERVICE_FROM_SRAMP("Select service from  S-RAMP"), REGISTER_EXISTING_SERVICE(
+			"Identify Service"), EVALUATE_TEST_RESULTS("Evaluate tests result"), SELECT_SERVICE_FROM_SRAMP("Select service from S-RAMP"), REGISTER_EXISTING_SERVICE(
 			"Register Existing Service");
 
 	private HumanTaskName(String name) {
