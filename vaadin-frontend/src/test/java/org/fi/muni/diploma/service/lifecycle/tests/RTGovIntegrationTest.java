d2f445a2ef3503bf08504961f365877ce7eff885
agiertli <agiertli@redhat.com>
2014-11-12 17:12:38 +0100

    minor changes to rtgov test

--- vaadin-frontend/src/test/java/org/fi/muni/diploma/service/lifecycle/tests/RTGovIntegrationTest.java
+++ vaadin-frontend/src/test/java/org/fi/muni/diploma/service/lifecycle/tests/RTGovIntegrationTest.java
@@ -1,8 +1,8 @@
 package org.fi.muni.diploma.service.lifecycle.tests;
 
-import java.io.IOException;
+import static org.junit.Assert.assertNotNull;
 
-import javax.validation.constraints.AssertTrue;
+import java.io.IOException;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
@@ -10,15 +10,13 @@ import org.junit.Test;
 import org.overlord.rtgov.activity.model.ActivityType;
 import org.switchyard.component.test.mixins.http.HTTPMixIn;
 
-import static org.junit.Assert.*;
-
 /**
  * Prerequisites:
  * 
- * RTGov and SwitchYard deployed on the same server
- * bean-service quickstart deployed
+ * RTGov and SwitchYard deployed on the same server & bean-service quickstart deployed
+ * 
  * @author osiris
- *
+ * 
  */
 public class RTGovIntegrationTest {
 	private static final String XML = "src/test/resources/xml/soap-request.xml";
@@ -26,23 +24,21 @@ public class RTGovIntegrationTest {
 	@Test
 	public void serviceInvocationTest() throws IOException, InterruptedException {
 
-		
 		long beforeInvocation = System.currentTimeMillis();
-		System.out.println("before"+beforeInvocation);
-		
-		boolean success = false;
-		
+		System.out.println("before" + beforeInvocation);
+
 		HTTPMixIn soapMixIn = new HTTPMixIn();
 		soapMixIn.initialize();
 
-		 soapMixIn.postFile("http://localhost:8480/quickstart-bean/OrderService", XML);
+		soapMixIn.postFile("http://localhost:8480/quickstart-bean/OrderService", XML);
 		soapMixIn.uninitialize();
-		
+
 		Thread.sleep(5000); // we need to let RTGov process the invocation
 		long afterInvocation = System.currentTimeMillis();
 
-		//now, we should see some request within this time period
-		java.net.URL queryUrl = new java.net.URL("http://localhost:8480/overlord-rtgov/activity/events?from="+beforeInvocation+"&to="+afterInvocation);
+		// now, we should see some request within this time period
+		java.net.URL queryUrl = new java.net.URL("http://localhost:8480/overlord-rtgov/activity/events?from=" + beforeInvocation + "&to="
+				+ afterInvocation);
 		java.net.HttpURLConnection connection = (java.net.HttpURLConnection) queryUrl.openConnection();
 		String userPassword = "admin" + ":" + "password1!";
 		String encoding = org.apache.commons.codec.binary.Base64.encodeBase64String(userPassword.getBytes());
@@ -61,7 +57,7 @@ public class RTGovIntegrationTest {
 		});
 		is.close();
 		System.out.println(activities.size());
-        assertNotNull(activities);
+		assertNotNull(activities);
 
 	}
 }
