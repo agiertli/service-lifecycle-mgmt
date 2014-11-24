56a18280306d942325e0d5ee63910bf1aa874bf9
agiertli <agiertli@redhat.com>
2014-11-25 00:32:29 +0100

    startup script which spawns all three JBoss JVMs

---
+++ installation/start.sh
@@ -0,0 +1,8 @@
+cd jvm3-soa-server/jboss-eap-6.3/bin
+./standalone.sh -Djboss.socket.binding.port-offset=400 --server-config=standalone-full.xml &
+#cd ../../..
+#cd jvm1-jbpm/jboss-eap-6.3/bin
+#./standalone.sh --server-config=standalone-full.xml -Dorg.jbpm.var.log.length=2147483647 &
+#cd ../../..
+#cd jvm2-service-lifecycle/jboss-eap-6.3/bin
+#./standalone.sh -Djboss.socket.binding.port-offset=200 &
