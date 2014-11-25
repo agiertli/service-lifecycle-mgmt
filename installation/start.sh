96dee7a052273a94ef0ad56f50a28e00c271249d
agiertli <agiertli@redhat.com>
2014-11-25 15:37:38 +0100

    startup script which supress all the jboss output and then open browser window with the application once servers are up

--- installation/start.sh
+++ installation/start.sh
@@ -1,8 +1,29 @@
 cd jvm3-soa-server/jboss-eap-6.3/bin
-./standalone.sh -Djboss.socket.binding.port-offset=400 --server-config=standalone-full.xml &
+./standalone.sh -Djboss.socket.binding.port-offset=400 --server-config=standalone-full.xml > /dev/null 2>&1 &
 cd ../../..
 cd jvm1-jbpm/jboss-eap-6.3/bin
-./standalone.sh --server-config=standalone-full.xml -Dorg.jbpm.var.log.length=2147483647 &
+./standalone.sh --server-config=standalone-full.xml -Dorg.jbpm.var.log.length=2147483647 > /dev/null 2>&1  &
 cd ../../..
 cd jvm2-service-lifecycle/jboss-eap-6.3/bin
-./standalone.sh -Djboss.socket.binding.port-offset=200 &
+./standalone.sh -Djboss.socket.binding.port-offset=200 > /dev/null 2>&1 &
+
+running="false";
+echo "..application is starting, wait up"
+
+while [ $running == "false" ]; do
+echo -n "."
+
+sleep 10
+if [[ "$(./jboss-cli.sh -c --controller=localhost:10199 --command='read-attribute server-state')" == "running" && "$(./jboss-cli.sh -c --controller=localhost:9999 --command='read-attribute server-state')" == "running" && "$(./jboss-cli.sh -c --controller=localhost:10399 --command='read-attribute server-state')" == "running" ]]; 
+     then running="true"; fi;
+done
+
+echo "application started successfully..opening"
+
+if which xdg-open > /dev/null
+then
+  xdg-open http://localhost:8280/service-lifecycle-mgmt/ > /dev/null 2>&1
+elif which gnome-open > /dev/null
+then
+  gnome-open http://localhost:8280/service-lifecycle-mgmt/ > /dev/null 2>&1
+fi
